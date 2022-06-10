a30bc82c9814f422874b5a9af053df0e06ef0aa7
FrancoBugnaaa <101563222+FrancoBugnaaa@users.noreply.github.com>
2022-06-10 13:51:21 -0300

    try stop while die

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -8,11 +8,12 @@ public class Timer : MonoBehaviour
     public Text txtTime;
     int TotalTime = 60;
     float TimeLeft = 0;
+    public Text Over;
 
     // Start is called before the first frame update
     void Start()
     {
-  
+        Over.enabled = false;
     }
 
     // Update is called once per frame
@@ -22,6 +23,14 @@ public class Timer : MonoBehaviour
         {
             TimeLeft = TotalTime - Mathf.Floor(Time.time);
             txtTime.text = TimeLeft.ToString();
+        }
+       if(TimeLeft == 0)
+        {
+            Over.enabled = true;
+        }
+       if(LifeCounter.countervida == 0)
+        {
+            txtTime.gameObject.SetActive(false); 
         }
     }
 }
